48a65a41163e7a28f5746989ac6fc1dccbdee97d
n.cerrone <nicholas.cerrone.99@gmail.com>
2019-10-08 16:44:34 +0200

    Second commit

--- app/src/main/java/com/example/googlesvunit/MainActivity.java
+++ app/src/main/java/com/example/googlesvunit/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.googlesvunit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     public EditText editTextUser;
     public EditText editTextPassword;
@@ -36,10 +37,17 @@ public class MainActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                openRegisterActivity();
             }
         });
 
 
     }
+
+    public void openRegisterActivity(){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+
+    }
+
 }
--- app/src/main/java/com/example/googlesvunit/SignUpActivity.java
+++ app/src/main/java/com/example/googlesvunit/SignUpActivity.java
@@ -1,11 +1,19 @@
 package com.example.googlesvunit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -13,6 +21,10 @@ public class SignUpActivity extends AppCompatActivity {
     public EditText editTextPassword;
     public Button confirmReigster;
     public Button login;
+
+    private FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +37,14 @@ public class SignUpActivity extends AppCompatActivity {
         confirmReigster = findViewById(R.id.buttonConfirmLogin);
         login = findViewById(R.id.buttonRegister);
 
+        mAuth = FirebaseAuth.getInstance();
+
+
+
         confirmReigster.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                registerUser();
             }
         });
 
@@ -36,6 +52,45 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+            }
+        });
+    }
+
+    public void registerUser(){
+        String username = editTextUser.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(username.isEmpty()){
+            editTextUser.setError("Email username required");
+            editTextUser.requestFocus();
+            return;
+        }
+
+        if(Patterns.EMAIL_ADDRESS.matcher(username).matches()){
+            editTextUser.setError("Email username invalid");
+            editTextUser.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password username required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length()<6){
+            editTextPassword.setError("Password username too short");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "User registered Successfull", Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
     }
